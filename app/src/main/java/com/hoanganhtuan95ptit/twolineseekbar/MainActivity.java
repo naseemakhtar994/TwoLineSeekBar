abcb1d8112490b3f659575a5acd30c184e2ade22
Hoang Anh Tuan <hoanganhtuan01101995@gmai.com>
2017-12-05 11:28:13 +0700

    add store

--- app/src/main/java/com/hoanganhtuan95ptit/twolineseekbar/MainActivity.java
+++ app/src/main/java/com/hoanganhtuan95ptit/twolineseekbar/MainActivity.java
@@ -1,7 +1,9 @@
 package com.hoanganhtuan95ptit.twolineseekbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hoanganhtuan95ptit.library.TwoLineSeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TwoLineSeekBar twoLineSeekBar = findViewById(R.id.twoLine);
+        twoLineSeekBar.setOnSeekChangeListener(new TwoLineSeekBar.OnSeekChangeListener() {
+            @Override
+            public void onSeekChanged(float value, float step) {
+
+            }
+
+            @Override
+            public void onSeekStopped(float value, float step) {
+
+            }
+        });
+
+        twoLineSeekBar.setOnSeekDownListener(new TwoLineSeekBar.OnSeekDownListener() {
+            @Override
+            public void onSeekDown() {
+
+            }
+        });
+
+        twoLineSeekBar.setOn
     }
 }
